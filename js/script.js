77ba9a3871b960a0a7dc31cc2942d8dc55a3db40
isferdinand <ikipkogey@kabarak.ac.ke>
2023-10-14 17:41:17 +0300

    displayed the showDetails

--- js/script.js
+++ js/script.js
@@ -85,7 +85,6 @@ const displayMovieDetails = async () => {
 </div>`;
 
   document.querySelector('#movie-details').appendChild(div);
-  console.log(movie);
 };
 
 //Display  popular tvshows
@@ -115,6 +114,68 @@ const displayPopularShows = async () => {
     const tvShow = document.querySelector('#popular-shows');
     tvShow.appendChild(div);
   });
+};
+
+//Display the show details to the DOM
+const displayShowDetails = async () => {
+  const seriesId = window.location.search.split('?')[1];
+
+  const show = await fetchData(`tv/${seriesId}`);
+
+  // Overlay for the background Image
+  displayBgImage('show', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    show.poster_path
+      ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}"/>`
+      : `<img src="../images/no-img.jpg" class="card-img-top" alt="${show.name}"/>`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>${show.overview}</p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}  
+    </ul>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit Show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+  <li><span class="text-secondary">Number Of Seasons:</span> ${
+    show.number_of_seasons
+  }</li>
+  <li><span class="text-secondary">Number Of Episodes:</span> ${
+    show.number_of_episodes
+  }</li>
+    <li>
+      <span class="text-secondary">Last Episode To Air:</span> ${
+        show.last_episode_to_air.name
+      }
+    </li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">${show.production_companies
+    .map((company) => company.name)
+    .join(', ')}</div>
+</div>
+  `;
+
+  document.querySelector('#show-details').appendChild(div);
 };
 
 //Display background images of movies or shows on details pages
@@ -188,7 +249,7 @@ const init = () => {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('show details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('search');
