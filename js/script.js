5b884648ef92a0ce970cf84c6b532ec4c6c52cef
isferdinand <ikipkogey@kabarak.ac.ke>
2023-10-14 16:44:38 +0300

    added background img movieDetails

--- js/script.js
+++ js/script.js
@@ -36,6 +36,9 @@ const displayMovieDetails = async () => {
 
   const movie = await fetchData(`movie/${movieId}`);
 
+  // Overlay for the background Image
+  displayBgImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.innerHTML = `
   <div class="details-top">
@@ -113,6 +116,28 @@ const displayPopularShows = async () => {
     tvShow.appendChild(div);
   });
 };
+
+//Display background images of movies or shows on details pages
+function displayBgImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
 
 // fetch data from movie API
 const fetchData = async (endpoint) => {
