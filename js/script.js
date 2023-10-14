cf3fdf7df43530ddc605d520f7ce6ca56fe02535
isferdinand <ikipkogey@kabarak.ac.ke>
2023-10-14 12:21:59 +0300

    displayed movie-details

--- js/script.js
+++ js/script.js
@@ -30,6 +30,61 @@ const displayPopularMovies = async () => {
   });
 };
 
+// Display the movie Details
+const displayMovieDetails = async () => {
+  const movieId = window.location.search.split('?')[1];
+
+  const movie = await fetchData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+      ${
+        movie.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.name}"/>`
+          : `<img src="../images/no-img.jpg" class="card-img-top" alt="${movie.name}"/>`
+      }
+      </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+       ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>${movie.overview}</p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${
+        movie.homepage
+      }" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+</div>
+<div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${movie.budget.toLocaleString(
+        'en-US'
+      )}</li>
+      <li><span class="text-secondary">Revenue:</span> $${movie.revenue.toLocaleString(
+        'en-US'
+      )}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime}</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${movie.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(', ')}</div>
+</div>`;
+
+  document.querySelector('#movie-details').appendChild(div);
+  console.log(movie);
+};
+
 //Display  popular tvshows
 const displayPopularShows = async () => {
   const { results } = await fetchData('tv/popular');
@@ -105,7 +160,7 @@ const init = () => {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('movie details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('show details');
